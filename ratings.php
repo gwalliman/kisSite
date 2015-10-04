5470b23ffdbdf3e76cf24041c5407a90ed862fbb
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 10:00:22 -0700

    Updating ratings

--- ratings.php
+++ ratings.php
@@ -6,10 +6,7 @@ include 'header_operationKiS.php';
 <?php
 try
 {
-  $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require options='--client_encoding=UTF8'");
-  echo('DB: ' . $db . '<br />');
-  $result = pg_exec($db, "select * from ratings");
-  echo('Results: ' . $result);
+  $db = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass sslmode=require options='--client_encoding=UTF8'");
 }
 catch(Exception $e)
 {
@@ -20,6 +17,14 @@ catch(Exception $e)
 	<div id="main" class="center">
 		<form id="tellStory" action="ratings.php" class="messageBox center">
       Give a rating of 1 to 5 indicating your experience with your listener.
+      <select name="ratingNum">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <input name="ratingId" id="ratingId" type="text" value="<?php echo $_GET['id']; ?>">
 			<input name="submitTell" id="submitTell" type="submit" value="Submit" class="submit bBlue">
 		</form>
 	</div>
