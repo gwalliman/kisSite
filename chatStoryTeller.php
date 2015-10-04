feb75a6eb3927c9322aa150423c6d9a31d3e859b
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 08:28:39 -0700

    Updating storyteller

--- chatStoryTeller.php
+++ chatStoryTeller.php
@@ -2,6 +2,10 @@
 include 'connectDB.php';
 include 'header_operationKiS.php';
 ?>
+<?php
+  $db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require options='--client_encoding=UTF8'");
+  echo('DB: ' . $db . '<br />');
+?>
 
 <h1>Chat interface for story teller</h1>
 
