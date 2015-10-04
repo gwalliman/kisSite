1871cb797db86f24eb4e9267b10f1c8375728ed6
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 11:51:49 -0700

    Updating

--- chatListener.php
+++ chatListener.php
@@ -1,25 +1,29 @@
 <?php
 include 'connectDB.php';
 include 'header_operationKiS.php';
+?>
+
+<div id="listenerInfo">
+  <!--<div>You (Rating:'.$rating.')</div>-->
+  <div>
+    List of open cases:';
+<?php
 
 try
 {
 	$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require options='--client_encoding=UTF8'");
-	$rating = pg_query($db, "SELECT * FROM ratings");//.$_GET['listenerUserNameSignIn'])."'";
+	$rating = pg_query($db, "SELECT * FROM ratings");
 	$cases = pg_query($db, "SELECT * FROM clients");
-	echo	'<div id="listenerInfo">
-				<div>You (Rating:'.$rating.')</div>
-				<div>List of open cases:';
+
+
 	echo '<ul>';
+
 	foreach($c as $cases)
 	{
 		echo '<li>' . $c . '</li>';
 	}
-	echo '</ul>
-				</div>
-			</div>
-			<!--div id="chatbox" class="chatbox"><h3>chatbox</h3></div>
-			<div class="stopFloat"></div-->';
+
+	echo '</ul>';
 }
 catch(Exception $e)
 {
@@ -27,8 +31,8 @@ catch(Exception $e)
 }
 
 ?>
-
-<h1>Chat interface for listener</h1>
+  </div>
+</div>';
 
 <?php
 include 'footer_operationKiS.php';
