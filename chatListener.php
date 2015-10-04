96235f101e0cd09c40c694532962b01667dd9876
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 11:54:28 -0700

    Updating

--- chatListener.php
+++ chatListener.php
@@ -6,23 +6,20 @@ include 'header_operationKiS.php';
 <div id="listenerInfo">
   <!--<div>You (Rating:'.$rating.')</div>-->
   <div>
-    List of open cases:';
+    List of open cases:
 <?php
 
 try
 {
-	$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require options='--client_encoding=UTF8'");
+  $db = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass sslmode=require options='--client_encoding=UTF8'");
 	$rating = pg_query($db, "SELECT * FROM ratings");
 	$cases = pg_query($db, "SELECT * FROM clients");
 
-
 	echo '<ul>';
-
-	foreach($c as $cases)
+  while($row = pg_fetch_array($cases))
 	{
-		echo '<li>' . $c . '</li>';
+		echo '<li>' $row['id'] . ' | ' . $row['subject'] . '</li>';
 	}
-
 	echo '</ul>';
 }
 catch(Exception $e)
