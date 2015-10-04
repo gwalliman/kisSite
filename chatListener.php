a75f1f80620f920f92135436d3769a992a612a87
Yong Zou <eric.yongjie.zou@gmail.com>
2015-10-04 09:26:20 -0700

    chatL

--- chatListener.php
+++ chatListener.php
@@ -1,6 +1,24 @@
 <?php
 include 'connectDB.php';
 include 'header_operationKiS.php';
+
+try
+{
+	$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass sslmode=require options='--client_encoding=UTF8'");
+	$rating = pg_exec($db, "select * from ratings where listener = '".$_GET['listenerUserNameSignIn'])."'";
+	$cases = pg_exec($db, "select * from clients");
+	echo	'<div id="listenerInfo" class="floatLeft">
+				<div>You (Rating:'.$rating.')</div>
+				<div>List of open cases:</div>
+			</div>
+			<div id="chatbox"></div>
+			<div class="stopFloat"></div>'
+}
+catch(Exception $e)
+{
+  echo($e);
+}
+
 ?>
 
 <h1>Chat interface for listener</h1>
