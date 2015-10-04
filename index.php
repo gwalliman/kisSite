1e35f4dede6b32214c960c3804f3578c3980d41e
Yong Zou <eric.yongjie.zou@gmail.com>
2015-10-03 22:40:46 -0700

    mess

--- index.php
+++ index.php
@@ -13,47 +13,43 @@
 </head>
 
 <body>
-	<div id="teamName" class="bBlue">
-		Operation KiS
-	</div>
-	
-	<div id="main" class="center">
+	<div id="main">
+		<div id="teamName" class="bBlue">
+			Operation KiS
+		</div>
+		
 		<div id="about">
 			Welcome to our site where if you need to be heard then we are here to listen. Here is a brief summary of what this site is all about. Depending on the purpose of your visit will determine which button you will click. So simple!
 		</div>
 		<div id="twoButton" class="center">
-			<div id="tell" class="button bBlue tile">
-				<a href="">I need to be heard</a>
-			</div>
-			<div id="listen" class="button bBlue tile">
-				<a href="">I am here to listen</a>
-			</div>
-			
-			<div id="tellStory" class="center">
-				Tell us your story
-				<div id="txtStory" class="bGrey">
-					haha
-				</div>
-				<div id="submitTell" class="floatRight">Submit</div>
-			</div>
-			
-			<div id="listenerSignIn" class="center">
-				Listener Sign-in
-				<input id="listenerUserName" class="bGrey" placeholder="Username">
-				<input id="listenerPassword" class="bGrey" placeholder="Password">
-				<div id="submitSignIn" class="floatRight">Submit</div>
-			</div>
+			<div id="tell" class="button1 bBlue">I need to be heard</div>
+			<div id="listen" class="button1 bBlue">I am here to listen</div>
+			<div class="stopFloat"></div>
 		</div>
-	</div>
-	
-	<div id="logoFooter">
-		<div id="logo" class="bOrange"></div>
-		<div id="mis">
-			Privacy Policy
-			Terms of Use
-			About Us
-			FAQ
-			Contact Us
+		
+		<form id="tellStory" class="messageBox center">
+			Tell us your story
+			<textarea id="txtStory" class="bGrey">
+			</textarea>
+			<input id="submitTell" type="submit" value="Submit" class="submit bBlue">
+		</form>
+		
+		<form id="listenerSignIn" class="messageBox center">
+			Listener Sign-in
+			<input id="listenerUserName" class="bGrey" placeholder="Username">
+			<input id="listenerPassword" class="bGrey" placeholder="Password">
+			<input id="submitSignIn" type="submit" value="Submit" class="submit bBlue">
+		</form>
+		
+		<div id="logoFooter" class="bBlue">
+			<div id="logo" class="floatLeft bOrange">logo</div>
+			<div id="mis" class="floatRight">
+				<div>Privacy Policy</div>
+				<div>Terms of Use</div>
+				About Us
+				FAQ
+				Contact Us
+			</div>
 		</div>
 	</div>
 </body>
