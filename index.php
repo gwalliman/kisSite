7494ad20b42850fb59064620881c80db2eeb4f0c
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 08:39:36 -0700

    Linking up forms

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 include 'header_operationKiS.php';
+include 'connectDB.php';
+?>
+
+<?php
+
+function chatStoryTeller()
+{
+  $db = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass sslmode=require options='--client_encoding=UTF8'");
+  echo('DB: ' . $db);
+  echo('Txt Story: ' . $_GET['txtStory']);
+}
+
+function chatListener()
+{
+  $db = pg_connect("host=$dbHost port=$dbPort dbname=$dbName user=$dbUser password=$dbPass sslmode=require options='--client_encoding=UTF8'");
+  echo('DB: ' . $db);
+  echo('ListenerUserName: ' . $_GET['listenerUserName']);
+  echo('ListenerEmail: ' . $_GET['listenerEmail']);
+  echo('ListenerPassword: ' . $_GET['listenerPassword']);
+}
+
 ?>
 	
 	<div id="main" class="center">
@@ -12,9 +33,10 @@ include 'header_operationKiS.php';
 			<div class="stopFloat"></div>
 		</div>
 		
-		<form id="tellStory" action="chatStoryTeller.php" class="messageBox center hidden">
+		<!--<form id="tellStory" action="chatStoryTeller.php" class="messageBox center hidden">-->
+		<form id="tellStory" action="chatStoryTeller()" class="messageBox center hidden">
 			Tell us your story
-			<textarea id="txtStory" class="bGrey center"></textarea>
+			<textarea name="txtStory" id="txtStory" class="bGrey center"></textarea>
 			<input id="submitTell" type="submit" value="Submit" class="submit bBlue">
 		</form>
 		
@@ -26,17 +48,17 @@ include 'header_operationKiS.php';
 		
 		<form id="listenerSignUp" action="regListener.php" method="get" class="messageBox center hidden">
 			Register as a Listener
-			<input id="listenerEmail" class="bGrey" placeholder="Email">
-			<input id="listenerUserName" class="bGrey" placeholder="Username">
-			<input id="listenerPassword" type="password" class="bGrey" placeholder="Password">
+			<input name="listenerEmail" id="listenerEmail" class="bGrey" placeholder="Email">
+			<input name="listenerUserName" id="listenerUserName" class="bGrey" placeholder="Username">
+			<input name="listenerPassword" id="listenerPassword" type="password" class="bGrey" placeholder="Password">
 			<input type="checkbox">I have read and agreed to the <a href="">Terms of use</a>
 			<input id="submitSignIn" type="submit" value="Submit" class="submit bBlue">
 		</form>
 		
 		<form id="listenerSignIn" action="chatListener.php" method="get" class="messageBox center hidden">
 			Listener Sign-in
-			<input id="listenerUserName" class="bGrey" placeholder="Username">
-			<input id="listenerPassword" class="bGrey" placeholder="Password">
+			<input name="listenerUserNameSignIn" id="listenerUserNameSignIn" class="bGrey" placeholder="Username">
+			<input name="listenerPasswordSignIn" id="listenerPasswordSignIn" class="bGrey" placeholder="Password">
 			<a href="">(Forgot password?)</a>
 			<input id="submitSignIn" type="submit" value="Submit" class="submit bBlue">
 		</form>
