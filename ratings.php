dd51ebeeecd117ce8b38929b058d25767e77b102
Garret Walliman <garret@garrets-mbp.mobile.asu.edu>
2015-10-04 08:51:48 -0700

    Debugging

--- ratings.php
+++ ratings.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'ec2-54-204-15-41.compute-1.amazonaws.com';
-$port = '5432';
-$dbname = 'd2k8bqie1ec0rk';
-$user = 'dhumuikvpxdsmu';
-$pass = 'NM5Twg9CM4QFsjynlz_3M1PFhz';
+$dbHost = 'ec2-54-204-15-41.compute-1.amazonaws.com';
+$dbPort = '5432';
+$dbName = 'd2k8bqie1ec0rk';
+$dbUser = 'dhumuikvpxdsmu';
+$dbPass = 'NM5Twg9CM4QFsjynlz_3M1PFhz';
 
 try
 {
