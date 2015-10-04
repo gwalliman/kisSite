b73279a23c4c7eb522f747f28195de422561edcf
Yong Zou <eric.yongjie.zou@gmail.com>
2015-10-03 20:56:45 -0700

    asdf

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="keywords" content="" />
+	<!--link rel="icon" type="image/png" href="http://example.com/myIcon.png" /-->
+	<link rel="stylesheet" type="text/css" href="css/index.css" />
+	<style></style>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<!--script src="../scripts_other/jquery-1.11.0.js"></script-->
+	<!--script src="scripts/SolEasy.js"></script-->
+</head>
+
+<body>
+	<div id="teamName" class="bBlue">
+		Operation KiS
+	</div>
+	
+	<div id="main" class="center">
+		<div id="about">
+			Welcome to our site where if you need to be heard then we are here to listen. Here is a brief summary of what this site is all about. Depending on the purpose of your visit will determine which button you will click. So simple!
+		</div>
+		<div id="twoButton">
+			<div class="button bBlue tile">
+				<a href="">I need to be heard</a>
+			</div>
+			<div id="tellStory" class="center hidden">
+				Tell us your story
+				</div id="txtStory" class="bGrey">
+					
+				</div>
+				<div id="Submit" class="floatRight">Submit</div>
+			</div>
+			<div class="button bBlue tile">
+				<a href="">I am here to listen</a>
+			</div>
+		</div>
+	</div>
+	
+	<div id="logoFooter">
+		<div id="logo" class="bOrange"></div>
+		<div id="mis">
+			Privacy Policy
+			Terms of Use
+			About Us
+			FAQ
+			Contact Us
+		</div>
+	</div>
+</body>
+
+<footer>
+</footer>
+</html>
---
+++ test2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="keywords" content="" />
+	<link rel="icon" type="image/png" href="http://example.com/myIcon.png" />
+	<link rel="stylesheet" type="text/css" href="css/SolEasy.css" />
+	<style></style>
+	<!--<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>-->
+	<script src="../scripts_other/jquery-1.11.0.js"></script>
+	<script src="scripts/SolEasy.js"></script>
+</head>
+
+<body>
+	<!-- Register/Login -->
+	<div id="userBar">
+		<!-- Small logo -->
+		<div id="sLogo">SolEasy</div>
+		<!-- -->
+		<div id="uControl">
+			<div id="setting">&#x2699;</div>
+			<div id="reg_log">Create account / Login</div>
+			<div id="uName">Welcome!</div>
+		</div>
+	</div>
+	<!-- Logo -->
+	<div>
+	</div>
+	
+	<div>Going Solar is SOLeasy!</div>
+	<div id="main" class="center">
+		<h2 class="center">You want information to:</h2>
+		<!-- Tile navigation -->
+		<div class="center square">
+			<div id="tileNav">
+				<div class="tile bOrange">
+					<a href="GoSolar.html">Go solar!</a>
+				</div>
+				<div class="tile bGreen">
+					<a href="StartBusiness.html">Start business in solar</a>
+				</div>
+				<div class="tile bBlue">
+					<a href="HowSolarWorks.html">Learn about how solar works</a>
+				</div>
+				<div class="tile bRed">
+					<a href="Forum.html">Know what others are thinking/discussing</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>Product/Services Comparison, get multiple quotes with on inquiry.</div>
+	<div>Rating system (Your options will also be commented or rated by other users.)</div>
+	<div>Suggestions: We don't assume or pretend we know everything. The products and services are developing rapidly, so do the user pool. We grow with you. To help us help you, please click here.</div>
+	<div>Disclaimer:</div>
+</body>
+
+<footer>
+
+</footer>
+</html>
